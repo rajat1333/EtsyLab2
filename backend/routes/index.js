47d7ea727223140ffd480339f00d04919a42ce05
rajat1333 <rajatmasurkar@gmail.com>
2022-02-20 10:18:06 -0800

    Created my sql database on aws RDS and connnected it to node application. Added proper dependencies for mysql and added code for connting to db

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var app = express();
+
+var con = mysql.createConnection({
+  host: "etsy.cdj42dsuxllh.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "admin1234",
+  port : 3306,
+  database : "etsy"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to mysql database!");
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
