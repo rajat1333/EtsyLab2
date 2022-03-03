07af2446920cb0ca7c5e8996c5ea2f29f20a77c5
rajat1333 <rajatmasurkar@gmail.com>
2022-03-02 19:08:16 -0800

    modified app.js to include previous login changes

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
+var signUpRouter = require('./routes/signUp');
+var homeRouter = require('./routes/home');
+var shopRouter = require('./routes/shop');
 var usersRouter = require('./routes/users');
-
+h
 var app = express();
+
+//use cors to allow cross origin resource sharing
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+//use express session to maintain session data
+app.use(session({
+  secret              : 'cmpe273_kafka_passport_mongo',
+  resave              : false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
+  saveUninitialized   : false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
+  duration            : 60 * 60 * 1000,    // Overall duration of Session : 30 minutes : 1800 seconds
+  activeDuration      :  5 * 60 * 1000
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +40,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/signUp', signUpRouter);
+app.use('/home', homeRouter);
+app.use('/shop', shopRouter);
 //app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -40,5 +60,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
