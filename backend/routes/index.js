d174f9c45e4638dcaceefc0bcf67dc7a980dd878
rajat1333 <rajatmasurkar@gmail.com>
2022-03-11 16:59:20 -0800

    removed reduncant code for mysql connection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,21 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var app = express();
-var constants = require("../config/config.json")
-
-var connPool = mysql.createPool({
-  host: constants.DB.host,
-  user: constants.DB.user,
-  password: constants.DB.password,
-  port : constants.DB.port,
-  database : constants.DB.database
-});
-
-connPool.getConnection(function(err) {
-  if (err) throw err;
-  console.log("Connected to mysql database!");
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
