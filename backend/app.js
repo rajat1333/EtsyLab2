0cbcf8a4faa507d7ce7e684d810419e93a699329
rajat1333 <rajatmasurkar@gmail.com>
2022-04-04 18:24:41 -0700

    added congig file for mongodb. Added dependencies in package.json and added code to connect to mongodb in app.js

--- backend/app.js
+++ backend/app.js
@@ -68,6 +68,23 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+const { mongoDB } = require('./config/config');
+const mongoose = require('mongoose');
 
+var options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  poolSize: 500,
+  bufferMaxEntries: 0
+};
+
+mongoose.connect(mongoDB, options, (err, res) => {
+  if (err) {
+      console.log(err);
+      console.log(`MongoDB Connection Failed`);
+  } else {
+      console.log(`MongoDB Connected`);
+  }
+});
 
 module.exports = app;
---
+++ backend/config/config.js
@@ -0,0 +1,5 @@
+const config = {
+    mongoDB: 'mongodb+srv://admin:admin@cluster0.zp1oj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
+
+module.exports = config;
