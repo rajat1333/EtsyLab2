2c1bb0b620048d36c9eb06ca7ed593f61effba5e
rajat1333 <rajatmasurkar@gmail.com>
2022-04-06 18:35:17 -0700

    added model files for purchase and shops

---
+++ backend/models/ShopModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var shopsSchema = new Schema({
+    name: {type: String, required: false},
+    email_id: {type: String, required: false},
+    shop_image: {type: String, required: false}
+},
+{
+    versionKey: false
+});
+
+const userModel = mongoose.model('user', shopsSchema);
+module.exports = userModel;
